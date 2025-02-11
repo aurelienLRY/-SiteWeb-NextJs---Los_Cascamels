6fc64fc6c29b9e8bac10f013abc2633fc3ff20f8
aurelienLRY <83220559+aurelienLRY@users.noreply.github.com>
2025-02-11 17:20:26 +0100

    Remove unused social media icon imports from Footer component

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,7 +4,6 @@
  */
 import React from "react";
 import Link from "next/link";
-import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
 
 /**
  * Interface pour les éléments de navigation
